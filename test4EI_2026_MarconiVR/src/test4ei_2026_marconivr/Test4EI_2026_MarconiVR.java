9cd2ad5b4ce391721b18e045b5d1c473136c9fbe
Luca Ronchetti <luca.ronchetti@marconiverona.edu.it>
2026-03-12 09:27:17 +0100

    primo commit

---
+++ test4EI_2026_MarconiVR/src/test4ei_2026_marconivr/Test4EI_2026_MarconiVR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test4ei_2026_marconivr;
+
+/**
+ *
+ * @author lronchet
+ */
+public class Test4EI_2026_MarconiVR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
